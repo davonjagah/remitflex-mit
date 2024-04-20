cf3f1369c31eb0ae5ce9412840e6463347408703
Aspirin <samuellyworld@gmail.com>
2024-04-20 08:20:59 +0100

    feat : add services🦾

---
+++ client/src/services/apiRoutes/paymentRoutes.ts
@@ -0,0 +1,9 @@
+import { hostRoute } from "../routesHelper";
+
+const paymentRoute = `${hostRoute}/payment`;
+
+export const currentRate = `${paymentRoute}/rate`;
+
+export const remitPayment = `${paymentRoute}/remit`;
+
+export const billPayment = `${paymentRoute}/bill`;
